b7475b33fe24e5d0099f38072fcaba37aca7ac97
sheryar shah <sheryar.shah53@yahoo.com>
2023-04-09 02:09:58 -0500

    Setup of springboot rest project and database connection setup

---
+++ src/main/java/com/gatech/payroll/EmployeePayrollSystem/EmployeePayrollSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gatech.payroll.EmployeePayrollSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollSystemApplication.class, args);
+	}
+
+}
